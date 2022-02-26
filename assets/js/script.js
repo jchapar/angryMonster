08f6d1ec0a8022fef843a6288256e44b3bb0bb64
josechaparro <josechaparro.dev@gmail.com>
2022-02-26 15:46:36 -0700

    added JS functionality to the theme toggle button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,23 @@ menuToggleClose.addEventListener("click", () =>
 );
 
 //Toggle theme
+const bodyElement = document.body;
+const themeToggleBtn = document.querySelector(".theme-toggle-btn");
+const currentTheme = localStorage.getItem("currentTheme");
+
+if (currentTheme) {
+  bodyElement.classList.add("theme-light");
+}
+
+themeToggleBtn.addEventListener("click", () => {
+  bodyElement.classList.toggle("theme-light");
+
+  if (bodyElement.classList.container("theme-light")) {
+    localStorage.setItem("currentTheme", "themeActive");
+  } else {
+    localStorage.removeItem("currentTheme", "themeActive");
+  }
+});
 
 //Show dropdown menu
 document.addEventListener("click", (e) => {
